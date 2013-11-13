1fb9cad2ec1248b5aacd7c051506f572460f0756
Armin Sadeghi <iamarmin@gmail.com>
2013-11-13 16:56:48 +1100

    Update init.el
    Add proxy authentication.

--- init.el
+++ init.el
@@ -1,3 +1,14 @@
+;; Proxy Authentication - Enable if required
+;;(setq url-proxy-services
+;;   '(("no_proxy" . "^\\(localhost\\|10.*\\)")
+;;     ("http" . "proxy.com:8080")
+;;     ("https" . "proxy.com:8080")))
+;;
+;;(setq url-http-proxy-basic-auth-storage
+;;    (list (list "proxy.com:8080"
+;;                (cons "Input your LDAP UID !"
+;;                      (base64-encode-string "LOGIN:PASSWORD")))))
+
 (require 'package)
 
 ;; Add package repos
