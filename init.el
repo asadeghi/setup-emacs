c8a9297fd462849affa555342b3e050569e4c42f
Armin Sadeghi <armin.sadeghi@bpmglobal.com>
2015-03-16 13:41:16 +1100

    Switch to Projectile instead of Helm.

--- init.el
+++ init.el
@@ -11,16 +11,16 @@
 
 ;; Add package repos
 (require 'package)
-(package-initialize)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
 ;;(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+(package-initialize)
 
 (when (not package-archive-contents)
   (package-refresh-contents))
 
 (defvar my-packages '(smartparens company
-                      helm helm-ls-git
+                      projectile
                       sml-mode
                       markdown-mode yaml-mode
                       scss-mode rainbow-mode web-mode
@@ -41,17 +41,17 @@
 (setq-default tab-width 4)
 (setq column-number-mode t)
 (tool-bar-mode -1)
-;;(helm-mode 1)
 ;;(load-theme 'solarized-[light|dark] t)
 (load-theme 'solarized-light t)
 (setq inhibit-startup-message t
   inhibit-startup-echo-area-message t) 
 (add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'after-init-hook 'projectile-global-mode)
 
 ;; Shortcuts
 ;;(global-set-key [(control \5)] (lambda () (interactive) (switch-to-buffer "*scratch*")))
 (global-set-key [f1] (lambda () (interactive) (switch-to-buffer "*scratch*")))
-(global-set-key [f5] 'helm-mini)
+(global-set-key [f5] 'projectile-find-file)
 (global-set-key [f7] 'start-kbd-macro)
 (global-set-key [f8] 'end-kbd-macro)
 (global-set-key [f9] 'call-last-kbd-macro)
@@ -80,20 +80,6 @@
 
 ;; Window size
 (when window-system (set-frame-size (selected-frame) 140 47))
-
-;; Redefine helm-mini
-(require 'helm-config)
-(require 'helm-ls-git)
-
-(defun helm-mini ()
-  "My additions (all git file) to Preconfigured `helm' lightweight version \(buffer -> recentf\)."
-  (interactive)
-  (require 'helm-files)
-  (helm-other-buffer '(
-    helm-c-source-buffers-list
-    helm-c-source-recentf
-    helm-c-source-ls-git)
-  "*helm mini*"))
 
 ;; -- Find emacs init file. You can also use: describe-variable user-init-file
 (defun find-user-init-file ()
