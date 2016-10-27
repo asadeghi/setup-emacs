393b97d3b47893f325325158958ba4db42f826c2
Armin Sadeghi <iamarmin@gmail.com>
2016-10-28 01:12:58 +1100

    Clean up emacs config.

--- init.el
+++ init.el
@@ -1,4 +1,7 @@
-;; Proxy Authentication - Enable if required
+;;; init.el --- Armin's Emacs configuration
+;;
+
+;; Proxy Authentication - Enable if you're behind an authenticated proxy.
 ;;(setq url-proxy-services
 ;;   '(("no_proxy" . "^\\(localhost\\|10.*\\)")
 ;;     ("http" . "proxy.com:8080")
@@ -9,7 +12,7 @@
 ;;                (cons "Input your LDAP UID !"
 ;;                      (base64-encode-string "LOGIN:PASSWORD")))))
 
-;; Add package repos
+;; Install packages
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
@@ -17,11 +20,12 @@
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
 ;;(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") t)
 
-;; Requires Emacs 24.4+ to work.
+;; Pinned packages require Emacs 24.4+ to work.
 (setq package-pinned-packages '((cider        . "melpa-stable")
                                 (clj-refactor . "melpa-stable")))
 
 (package-initialize)
+;; Update package archive if required.
 (when (not package-archive-contents)
   (package-refresh-contents))
  
@@ -29,7 +33,7 @@
                       smartparens
                       company ;; Completion framework
                       projectile ;; Project interaction
-                      ag
+                      ag ;; Silver-Searcher-Ag
                       sml-mode
                       markdown-mode
                       yaml-mode
@@ -58,89 +62,70 @@
 ;; Additional package path
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/modules"))
 
-;; Misc settings
+;; Always load newest compiled files.
+(setq load-prefer-newer t)
+
+;; Reduce the frequency of garbage collection by making it happen on
+;; each 50MB of allocated data (the default is on every 0.76MB)
+(setq gc-cons-threshold 50000000)
+
+;; Warn when opening files bigger than 100MB
+(setq large-file-warning-threshold 100000000)
+
+;; Disable the toolbar, waste of space.
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+
+;; Disable blink cursor.
+(blink-cursor-mode -1)
+
+;; Disable annoying bell ring.
+(setq ring-bell-function 'ignore)
+
+;; Misc settings.
 (cua-mode)
 (rainbow-mode)
 (global-hl-line-mode 1)
 (setq make-backup-files nil)
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
-(setq column-number-mode t)
-(tool-bar-mode -1)
 (setq create-lockfiles nil)
 (setq inhibit-startup-message t
       inhibit-startup-echo-area-message t)
 (add-hook 'after-init-hook 'projectile-global-mode)
 
-;; --- Package config ---
+;; Scroll one line at a time with mouse scroll wheel, no acceleration
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1) ((control) . nil)))
+(setq mouse-wheel-progressive-speed nil)
 
-; Dumb-jump jumps to definition of symbols: C-M g , and back: C-M p
-(use-package dumb-jump
-             :defer 4
-             :config
-             (dumb-jump-mode))
+;; Nice scrolling
+(setq scroll-margin 0
+      scroll-conservatively 100000
+      scroll-preserve-screen-position 1)
 
-(use-package company
-             :defer 3
-             :config
-             (global-company-mode))
+;; Mode line settings.
+(line-number-mode t)
+(column-number-mode t)
 
-(use-package ace-jump-mode
-             :defer t
-             :bind ("C-0" . ace-jump-mode))
+;; Shorthand for questions.
+(defalias 'yes-or-no-p 'y-or-n-p)
 
-;; Configure Ag. Ensure you have the silversearcher-ag package installed.
-(use-package ag
-             :defer t
-             :config
-             (setq ag-highlight-search t)
-             (setq ag-reuse-buffers 't)
-             (setq ag-arguments (list "--smart-case" "--column")))
+;; Set buffer title.
+(setq-default frame-title-format
+   (list '((buffer-file-name " %f"
+             (dired-directory
+              dired-directory
+              (revert-buffer-function " %b"
+              ("%b - Dir:  " default-directory)))))))
 
-;; Configure org-present for presentation of slides using Emacs.
-;; M-x org-present
-;; C-c C-q (org-present-quit)
-(use-package org-present
-             :defer 3
-             :init
-             (add-hook 'org-present-mode-hook
-               (lambda ()
-                 (org-present-big)
-                 (org-display-inline-images)
-                 (org-present-hide-cursor)
-                 (org-present-read-only)))
-             (add-hook 'org-present-mode-quit-hook
-               (lambda ()
-                 (org-present-small)
-                 (org-remove-inline-images)
-                 (org-present-show-cursor)
-                 (org-present-read-write))))
+;; Coding system
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
 
-(use-package exec-path-from-shell
-             :defer 1
-             :config (when (memq window-system '(mac ns))
-                           (exec-path-from-shell-initialize)))
-
-(use-package my-clojure
-             :defer 1)
-
-(use-package setup-smartparens
-             :defer 1)
-
-;; Shortcuts
-;;(global-set-key [(control \5)] (lambda () (interactive) (switch-to-buffer "*scratch*")))
-(global-set-key [f1] (lambda () (interactive) (switch-to-buffer "*scratch*")))
-(global-set-key [f2] (lambda () (interactive) (find-file "~/notes.txt")))
-(global-set-key [f3] 'query-replace)
-(global-set-key [f4] 'projectile-ag)
-(global-set-key [f5] 'projectile-find-file)
-(global-set-key [f6] 'start-kbd-macro)
-(global-set-key [f7] 'end-kbd-macro)
-(global-set-key [f8] 'call-last-kbd-macro)
-(global-set-key (kbd "<f9>") 'whitespace-mode)
-(global-set-key (kbd "C-8") 'move-cursor-previous-pane)
-(global-set-key (kbd "C-9") 'move-cursor-next-pane)
-;(global-set-key (kbd "C-0") 'ace-jump-mode)
+;; Replace buffer-menu with ibuffer
+(global-set-key (kbd "C-x C-b") #'ibuffer)
 
 ;; Easier window navigation
 (defun move-cursor-next-pane ()
@@ -156,21 +141,9 @@
 ;; Theme setup.
 (load-theme 'deeper-blue t)
 
-;; Scroll one line at a time with mouse scroll wheel, no acceleration
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1) ((control) . nil)))
-(setq mouse-wheel-progressive-speed nil)
-
 ;; Disable SASS auto-compilation, and integrate with rainbow-mode.
 (setq scss-compile-at-save nil)
 (add-hook 'css-mode-hook 'rainbow-mode)
-
-;; Set buffer title
-(setq-default frame-title-format
-   (list '((buffer-file-name " %f"
-             (dired-directory
-              dired-directory
-              (revert-buffer-function " %b"
-              ("%b - Dir:  " default-directory)))))))
 
 ;; Window font and size
 ;; Font size is in 1/10pt, so 100 will give you 10pt.
@@ -186,9 +159,6 @@
 ;; Fontify current and future frames
 (fontify-frame nil)
 (push 'fontify-frame after-make-frame-functions)
-
-;; Shorthand for questions
-(defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; Find emacs init file. You can also use: describe-variable user-init-file
 (defun find-user-init-file ()
@@ -235,3 +205,77 @@
 ;; Configure Auto-Complete
 ;;(require 'auto-complete-config)
 (ac-config-default)
+;;(setq ac-ignore-case nil)
+;;(add-to-list 'ac-modes 'enh-ruby-mode)
+;;(add-to-list 'ac-modes 'web-mode)
+
+;; Useful keybindings
+(global-set-key [f1] (lambda () (interactive) (switch-to-buffer "*scratch*")))
+(global-set-key [f2] 'query-replace)
+(global-set-key [f3] 'dumb-jump-go)
+(global-set-key [f4] 'projectile-ag)
+(global-set-key [f5] 'projectile-find-file)
+(global-set-key [f6] 'start-kbd-macro)
+(global-set-key [f7] 'end-kbd-macro)
+(global-set-key [f8] 'call-last-kbd-macro)
+(global-set-key (kbd "<f9>") 'whitespace-mode)
+(global-set-key (kbd "C-1") (lambda () (interactive) (find-file "~/notes.txt")))
+(global-set-key (kbd "C-8") 'move-cursor-previous-pane)
+(global-set-key (kbd "C-9") 'move-cursor-next-pane)
+;(global-set-key (kbd "C-0") 'ace-jump-mode)
+
+; Dumb-jump jumps to definition of symbols: C-M g , and back: C-M p
+(use-package dumb-jump
+             :defer 4
+             :config
+             (dumb-jump-mode))
+
+(use-package company
+             :defer 3
+             :config
+             (global-company-mode))
+
+(use-package ace-jump-mode
+             :defer t
+             :bind ("C-0" . ace-jump-mode))
+
+;; Configure Ag. Ensure you have the silversearcher-ag package installed.
+(use-package ag
+             :defer t
+             :config
+             (setq ag-highlight-search t)
+             (setq ag-reuse-buffers 't)
+             (setq ag-arguments (list "--smart-case" "--column"))
+             )
+
+;; Configure org-present for presentation of slides using Emacs.
+;; M-x org-present
+;; C-c C-q (org-present-quit)
+(use-package org-present
+             :defer 3
+             :init
+             (add-hook 'org-present-mode-hook
+               (lambda ()
+                 (org-present-big)
+                 (org-display-inline-images)
+                 (org-present-hide-cursor)
+                 (org-present-read-only)))
+             (add-hook 'org-present-mode-quit-hook
+               (lambda ()
+                 (org-present-small)
+                 (org-remove-inline-images)
+                 (org-present-show-cursor)
+                 (org-present-read-write))))
+
+(use-package exec-path-from-shell
+             :defer 1
+             :config (when (memq window-system '(mac ns))
+                           (exec-path-from-shell-initialize)))
+
+(use-package my-clojure
+             :defer 1)
+
+(use-package setup-smartparens
+             :defer 1)
+
+;; EOF
